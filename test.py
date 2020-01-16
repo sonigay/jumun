ede2c67150d2c023378441c82949f54465c7b560
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-16 22:35:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ async def on_message(message):
     if message.content.startswith('!주문'):
         if message.channel.is_private and message.author.id != "667338660420780032":
             await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content[4:] + " 주문이 접수되었습니다.")
-            await client.send_message(message.channel, '주문이 정상적으로 접수되었습니다. \n부득이한경우 개인답변 드리겠습니다.')
+            await client.send_message(message.channel, '\n주문이 정상적으로 접수되었습니다. \n부득이한경우 개인답변 드리겠습니다.')
             
     if message.content.startswith('!주문'):
         if message.channel.is_private and message.author.id != "667338660420780032":
