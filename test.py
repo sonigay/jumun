ed4f87473b1f396b5e3cd33caed4776e8aba9aae
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-18 02:46:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -51,32 +51,7 @@ async def on_message(message):
             await client.send_message(member, "홍팀장 개인답변 : " + message.content[23:])
             
             
-    if message.content == '!명령어':
-        command_list = ''
-        command_list += '!모델명\n'     #!모델명
-        command_list += '!재고 [모델명]\n'     #!재고+모델명
-        command_list += '!재고 [구단위]\n'     #!재고+구단위
-        command_list += '!퀵비 [동단위/동단위]\n'     #!퀵비
-        command_list += '!동판 [동판신규]\n'     #!동판
-        command_list += '!동판 [동판기변]\n'     #!동판
-        command_list += '!동판 [소호신규]\n'     #!동판
-        command_list += '!동판 [소호기변]\n'     #!동판
-        command_list += '!동판 [후결합]\n'     #!동판
-        command_list += '!동판 [재약정기존]\n'     #!동판
-        command_list += '!동판 [재약정전환]\n'     #!동판
-        command_list += '!동판 [재약정단독기존]\n'     #!동판
-        command_list += '!동판 [재약정단독전환]\n'     #!동판
-        command_list += '!동판 [단독]\n'     #!동판
-        embed = discord.Embed(
-            title = ":keyboard: 명령어",
-            description= '```' + command_list + '```',
-            color=0xFFD5B4
-            )
-        embed.add_field(
-            name=":radio: 기타채널 명령어 ",
-            value= '```!주문 [주문넣을 단말기및 요청글] 채널:재고신청봇 개인메시지 \n!단가 [모델명 요금제군 유형] 채널:무선정책조회\n!외국인단가 [모델명 요금제군 유형] 채널:외국인정책조회\n```'
-            )
-        await client.send_message(message.channel, embed=embed)
+
 
                         
 access_token = os.environ["BOT_TOKEN"]
