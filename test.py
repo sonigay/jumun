7ce145b0166f5f922122ca8ddf718f8d5b4bc52f
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-29 19:21:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -50,6 +50,10 @@ async def on_message(message):
             member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
             await client.send_message(member, "홍팀장 개인답변 : " + message.content[23:])
             
+    
+    if message.content.startswith('!정책표'):
+        webbrowser.open_new("https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml")
+        await client.send_message(client.get_channel("672022974223876096"), message.author.display_name + "(" + message.author.id + ") : " + message.content[4:])    
             
 
 
