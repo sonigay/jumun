f6240ef2846ad74dcfd60785f67a0fb99ac07e49
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-29 22:39:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ async def on_message(message):
             gc = gspread.authorize(creds)
             wks = gc.open('오전재고').worksheet('재고주문')
             wks.insert_row([message.author.display_name, message.content[4:]], 3)
-	    await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content[4:] + " 주문이 접수되었습니다. 부득이한경우 개인답변 드리겠습니다.")
+            await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content[4:] + " 주문이 접수되었습니다. 부득이한경우 개인답변 드리겠습니다.")
             await client.send_message(client.get_channel("667343258296254464"), message.author.display_name + "(" + message.author.id + ") : " + message.content[4:])
             
 	
