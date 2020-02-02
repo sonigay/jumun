78e6f508c413892fc6c1cfdf53350aa1ed10f1da
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-02 19:46:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ async def on_message(message):
    
     if message.content.startswith('!주문'):
         
-        curruntTime = datetime.datetime.now() + datetime.timedelta(hours = 9))
+        curruntTime = datetime.datetime.now() + datetime.timedelta(hours = 9)
         krnow = curruntTime.strftime('%Y년%m월%d일 %H시%M분%S초')
         gc = gspread.authorize(creds)
         wks = gc.open('오전재고').worksheet('재고주문')
