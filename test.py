640ebcb4c3764d1b29330c7f33bc546c9360bcb7
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-05 21:34:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -70,8 +70,8 @@ async def on_message(message):
     if message.content == '!정책표':
         command_list = ''
         command_list += 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml'     #!링크
-	url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml'
-	webbrowser.open_new(url)
+        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml'
+        webbrowser.open_new(url)
         embed = discord.Embed(
             title = ":bar_chart: 정책표링크",
             description= command_list,
