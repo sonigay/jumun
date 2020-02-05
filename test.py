e1a9856ec36765e48ed2daaf60b5b1edffa909fc
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-05 21:53:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -71,14 +71,14 @@ async def on_message(message):
         command_list = ''
         command_list += 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml'     #!링크
         url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmWzJs5QX3i2Q5LaOugdA7NoxLZ3O_fQZAyYMejopSp0u-nuoe9iaQREswxmanQSbXs1Swm_ukKaJ7/pubhtml'
-        webbrowser.open_new(url)
+        web = webbrowser.open_new(url)
         embed = discord.Embed(
             title = ":bar_chart: 정책표링크",
             description= command_list,
             color=0xf29886
             )
         await client.send_message(client.get_channel("672022974223876096"), message.author.display_name + "(" + message.author.id + message.channel.name + ") : 정책표출력!! " + message.content[4:])
-        await client.send_message(message.channel, embed=embed)
+        await client.send_message(message.channel, web, embed=embed)
 
  
                         
