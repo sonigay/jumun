7f4a3ec8ef9cbc9d82c1320e7c33f882bdf8849f
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-08 21:13:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ async def on_member_join(member):
     sleep(1)	
     fmt = '{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다. '
     channel = member.server.get_channel("661832869521391646")
-    return await client.send_message(channel, embed=discord.Embed(fmt.format(member, member.server))
+    return await client.send_message(channel, embed=discord.Embed(fmt.format(member, member.server)))
 
 
 
